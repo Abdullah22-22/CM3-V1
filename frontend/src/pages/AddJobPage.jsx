e78d3d4de287b4287a38aaf19234cc8f4db198ca
Armin Khoramidastjerdi <44828001+arminkhorami@users.noreply.github.com>
2026-03-06 11:02:51 +0200

    edit Add jobPage

--- frontend/src/pages/AddJobPage.jsx
+++ frontend/src/pages/AddJobPage.jsx
@@ -1,26 +1,45 @@
 import { useState } from "react";
+import jobsData from "../data/job.data.json";
 import { addJob } from "../api/jobApi";
 
-export default function AddJobPage() {
+const AddJobPage = () => {
   const [title, setTitle] = useState("");
   const [companyName, setCompanyName] = useState("");
   const [city, setCity] = useState("");
   const [state, setState] = useState("");
+  const [salary, setSalary] = useState("");
+  const [experienceLevel, setExperienceLevel] = useState("Entry");
   const [message, setMessage] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     const jobData = {
       title,
       company: { name: companyName },
       location: { city, state },
-      salary: 0,
-      experienceLevel: "Entry",
+      salary: parseInt(salary),
+      experienceLevel,
       status: "open"
     };
+
+    //send API
     const res = await addJob(jobData);
-    if (res.success) setMessage("Job added successfully!");
-    else setMessage("Error adding job");
+
+    if (res.success) {
+      setMessage("Job added successfully!");
+    } else {
+      setMessage("API unavailable, fallback to JSON");
+      jobsData.push(jobData);
+    }
+
+    // reset form start
+    setTitle("");
+    setCompanyName("");
+    setCity("");
+    setState("");
+    setSalary("");
+    setExperienceLevel("Entry");
   };
 
   return (
@@ -31,9 +50,17 @@ export default function AddJobPage() {
         <input placeholder="Company Name" value={companyName} onChange={e => setCompanyName(e.target.value)} required/>
         <input placeholder="City" value={city} onChange={e => setCity(e.target.value)} required/>
         <input placeholder="State" value={state} onChange={e => setState(e.target.value)} required/>
+        <input placeholder="Salary" value={salary} onChange={e => setSalary(e.target.value)} required/>
+        <select value={experienceLevel} onChange={e => setExperienceLevel(e.target.value)}>
+          <option value="Entry">Entry</option>
+          <option value="Mid">Mid</option>
+          <option value="Senior">Senior</option>
+        </select>
         <button type="submit">Add Job</button>
       </form>
       {message && <p>{message}</p>}
     </div>
   );
-}
+};
+
+export default AddJobPage;
